781e75fa3ad06ac213f3d5dfc86eb1acead3b89b
Tamim <tamim.hossen@transcombd.com>
2025-09-11 13:01:47 +0600

    done final

--- ams.sql
+++ ams.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate
-MySQL - 8.0.30 : Database - cpanel_rnd
+MySQL - 8.0.31-google : Database - access_management
 *********************************************************************
 */
 
@@ -28,14 +28,13 @@ CREATE TABLE `business_units` (
   `updated_on` datetime DEFAULT NULL,
   `updated_by` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `business_units` */
 
 insert  into `business_units`(`id`,`sbu_prefix`,`sbu_name`,`sbu_full_name`,`sbu_location`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'10','TCL','Transcom Limited','Gulshan 2',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(2,'11','SKF','SKF','Bananai',1,'',0,'','2025-07-15 18:10:17',NULL,'2025-07-15 18:11:39',NULL),
-(3,'19','PALO','Prothom Alo','Karwan Bazar',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+(1,'10','TCL','Transcom Limited','Gulshan 2, Dhaka',1,'',0,'','2025-09-11 17:03:40',NULL,'2025-09-11 17:03:40',NULL),
+(2,'11','SKF','Eskayef Pharma','Banani, Dhaka',1,'',0,'','2025-09-11 12:26:44',NULL,'2025-09-11 12:26:44',NULL);
 
 /*Table structure for table `projects` */
 
@@ -54,15 +53,13 @@ CREATE TABLE `projects` (
   `updated_on` datetime DEFAULT NULL,
   `updated_by` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `projects` */
 
 insert  into `projects`(`id`,`cid`,`project_id`,`project_name`,`project_description`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'TCL','cpanel','cpanel','Control Panel',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(2,'PALO',NULL,'surjo','surjo',1,'',0,'','2025-07-15 18:54:40',NULL,'2025-07-15 19:45:08',NULL),
-(3,'PALO',NULL,'circulation','Circulation',0,'',0,'','2025-07-15 19:45:40',NULL,'2025-07-15 19:45:40',NULL),
-(4,'SKF',NULL,'expanse','Expanse',1,'',0,'','2025-07-15 19:45:54',NULL,'2025-07-15 19:45:54',NULL);
+(1,'TCL','ams','ams','Access Management System',1,'',0,'','2025-09-11 17:04:15',NULL,'2025-09-11 17:04:15',NULL),
+(2,'SKF','expense','expense','Asset Tracking System',1,'',0,'','2025-09-11 12:27:12',NULL,'2025-09-11 12:27:12',NULL);
 
 /*Table structure for table `u_role_has_tasks` */
 
@@ -85,17 +82,26 @@ CREATE TABLE `u_role_has_tasks` (
   `updated_on` datetime DEFAULT NULL,
   `updated_by` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `u_role_has_tasks` */
 
 insert  into `u_role_has_tasks`(`id`,`cid`,`pid`,`role_id`,`role_name`,`task_id`,`task_name`,`group_id`,`group_name`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(22,'TCL',NULL,'1','super_admin','2','business_units_create',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
-(23,'TCL',NULL,'1','super_admin','3','role_management',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
-(24,'TCL',NULL,'1','super_admin','4','user_create',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
-(25,'TCL',NULL,'1','super_admin','6','user_delete',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
-(26,'TCL',NULL,'1','super_admin','5','user_view',NULL,NULL,1,'',0,'','2025-07-16 16:20:06',NULL,'2025-07-16 16:20:06',NULL),
-(27,'TCL',NULL,'3','normal_user','5','user_view',NULL,NULL,1,'',0,'','2025-07-16 16:24:46',NULL,'2025-07-16 16:24:46',NULL);
+(11,'TCL',NULL,'1','system_admin','1','company_management',NULL,NULL,1,'',0,'','2025-09-11 17:43:55',NULL,'2025-09-11 17:43:55',NULL),
+(12,'TCL',NULL,'1','system_admin','2','project_management',NULL,NULL,1,'',0,'','2025-09-11 17:43:55',NULL,'2025-09-11 17:43:55',NULL),
+(13,'TCL',NULL,'1','system_admin','5','role_manage',NULL,NULL,1,'',0,'','2025-09-11 17:43:55',NULL,'2025-09-11 17:43:55',NULL),
+(14,'TCL',NULL,'1','system_admin','3','task_group_manage',NULL,NULL,1,'',0,'','2025-09-11 17:43:55',NULL,'2025-09-11 17:43:55',NULL),
+(15,'TCL',NULL,'1','system_admin','4','task_manage',NULL,NULL,1,'',0,'','2025-09-11 17:43:55',NULL,'2025-09-11 17:43:55',NULL),
+(16,'TCL',NULL,'1','system_admin','6','user_create',NULL,NULL,1,'',0,'','2025-09-11 17:43:55',NULL,'2025-09-11 17:43:55',NULL),
+(17,'TCL',NULL,'1','system_admin','8','user_delete',NULL,NULL,1,'',0,'','2025-09-11 17:43:55',NULL,'2025-09-11 17:43:55',NULL),
+(18,'TCL',NULL,'1','system_admin','7','user_view',NULL,NULL,1,'',0,'','2025-09-11 17:43:55',NULL,'2025-09-11 17:43:55',NULL),
+(25,'TCL',NULL,'2','admin','5','role_manage',NULL,NULL,1,'',0,'','2025-09-11 18:09:48',NULL,'2025-09-11 18:09:48',NULL),
+(26,'TCL',NULL,'2','admin','3','task_group_manage',NULL,NULL,1,'',0,'','2025-09-11 18:09:48',NULL,'2025-09-11 18:09:48',NULL),
+(27,'TCL',NULL,'2','admin','4','task_manage',NULL,NULL,1,'',0,'','2025-09-11 18:09:48',NULL,'2025-09-11 18:09:48',NULL),
+(28,'TCL',NULL,'2','admin','6','user_create',NULL,NULL,1,'',0,'','2025-09-11 18:09:48',NULL,'2025-09-11 18:09:48',NULL),
+(29,'TCL',NULL,'2','admin','8','user_delete',NULL,NULL,1,'',0,'','2025-09-11 18:09:48',NULL,'2025-09-11 18:09:48',NULL),
+(30,'TCL',NULL,'2','admin','7','user_view',NULL,NULL,1,'',0,'','2025-09-11 18:09:48',NULL,'2025-09-11 18:09:48',NULL),
+(32,'SKF',NULL,'3','sysadmin','9','vendor_create',NULL,NULL,1,'',0,'','2025-09-11 12:54:31',NULL,'2025-09-11 12:54:31',NULL);
 
 /*Table structure for table `u_roles` */
 
@@ -119,8 +125,9 @@ CREATE TABLE `u_roles` (
 /*Data for the table `u_roles` */
 
 insert  into `u_roles`(`id`,`cid`,`pid`,`role_name`,`role_description`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'TCL','cpanel','super_admin','Super Admin',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3,'TCL','surjo','normal_user','normal user',1,'',0,'','2025-07-15 23:50:48',NULL,'2025-07-15 23:54:15',NULL);
+(1,'TCL','ams','system_admin','System Admin',1,'',0,'','2025-09-11 17:14:12',NULL,'2025-09-11 17:14:12',NULL),
+(2,'TCL','ams','admin','Admin',1,'',0,'','2025-09-11 17:14:20',NULL,'2025-09-11 17:14:20',NULL),
+(3,'SKF','expense','sysadmin','main role',1,'',0,'','2025-09-11 12:28:48',NULL,'2025-09-11 12:28:48',NULL);
 
 /*Table structure for table `u_task_group` */
 
@@ -139,14 +146,17 @@ CREATE TABLE `u_task_group` (
   `updated_on` datetime DEFAULT NULL,
   `updated_by` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `u_task_group` */
 
 insert  into `u_task_group`(`id`,`cid`,`pid`,`group_name`,`group_description`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'TCL','cpanel','access_management','Access Management',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(2,'TCL','cpanel','user_management','User management',1,'',0,'','2025-07-15 20:07:53',NULL,'2025-07-15 20:11:50',NULL),
-(3,'TCL','cpanel','task_management','task_management',1,'',0,'','2025-07-15 20:09:15',NULL,'2025-07-15 22:20:57',NULL);
+(1,'TCL','ams','company','Company',1,'',0,'','2025-09-11 17:04:48',NULL,'2025-09-11 17:04:48',NULL),
+(2,'TCL','ams','project','Project',1,'',0,'','2025-09-11 17:05:03',NULL,'2025-09-11 17:05:03',NULL),
+(3,'TCL','ams','task_management','Task Management',1,'',0,'','2025-09-11 17:05:31',NULL,'2025-09-11 17:05:31',NULL),
+(4,'TCL','ams','role_management','Role Management',1,'',0,'','2025-09-11 17:05:51',NULL,'2025-09-11 17:05:51',NULL),
+(5,'TCL','ams','user_management','User Management',1,'',0,'','2025-09-11 17:06:11',NULL,'2025-09-11 17:06:11',NULL),
+(6,'SKF','expense','master_setup','Setup Master ',1,'',0,'','2025-09-11 12:27:51',NULL,'2025-09-11 12:27:51',NULL);
 
 /*Table structure for table `u_tasks` */
 
@@ -167,16 +177,20 @@ CREATE TABLE `u_tasks` (
   `updated_on` datetime DEFAULT NULL,
   `updated_by` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `u_tasks` */
 
 insert  into `u_tasks`(`id`,`cid`,`pid`,`task_name`,`task_description`,`group_id`,`group_name`,`status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(2,'TCL','cpanel','business_units_create','business_units_create','1','access_management',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3,'TCL','cpanel','role_management','role_management','1','access_management',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(4,'TCL','cpanel','user_create','create user','2','user_management',1,'',0,'','2025-07-16 16:03:23',NULL,'2025-07-16 16:03:23',NULL),
-(5,'TCL','cpanel','user_view','View user','2','user_management',1,'',0,'','2025-07-16 16:03:36',NULL,'2025-07-16 16:03:36',NULL),
-(6,'TCL','cpanel','user_delete','Delete User','2','user_management',1,'',0,'','2025-07-16 16:17:26',NULL,'2025-07-16 16:17:26',NULL);
+(1,'TCL','ams','company_management','Company management Create, Edit, Delete','1','company',1,'',0,'','2025-09-11 17:06:59',NULL,'2025-09-11 17:06:59',NULL),
+(2,'TCL','ams','project_management','Project management','2','project',1,'',0,'','2025-09-11 17:10:22',NULL,'2025-09-11 17:10:22',NULL),
+(3,'TCL','ams','task_group_manage','Task group manage','3','task_management',1,'',0,'','2025-09-11 17:11:59',NULL,'2025-09-11 17:11:59',NULL),
+(4,'TCL','ams','task_manage','Task manage','3','task_management',1,'',0,'','2025-09-11 17:12:19',NULL,'2025-09-11 17:12:19',NULL),
+(5,'TCL','ams','role_manage','Role manage','4','role_management',1,'',0,'','2025-09-11 17:12:36',NULL,'2025-09-11 17:12:36',NULL),
+(6,'TCL','ams','user_create','user create','5','user_management',1,'',0,'','2025-09-11 17:12:59',NULL,'2025-09-11 17:12:59',NULL),
+(7,'TCL','ams','user_view','user view','5','user_management',1,'',0,'','2025-09-11 17:30:26',NULL,'2025-09-11 17:30:26',NULL),
+(8,'TCL','ams','user_delete','User Delete','5','user_management',1,'',0,'','2025-09-11 17:30:36',NULL,'2025-09-11 17:30:36',NULL),
+(9,'SKF','expense','vendor_create','Access for Vendor Create','6','master_setup',1,'',0,'','2025-09-11 12:28:15',NULL,'2025-09-11 12:28:15',NULL);
 
 /*Table structure for table `users` */
 
@@ -189,11 +203,12 @@ CREATE TABLE `users` (
   `last_name` varchar(100) DEFAULT NULL,
   `full_name` varchar(200) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
+  `username` varchar(100) NOT NULL DEFAULT '',
   `mobile` varchar(20) DEFAULT NULL,
   `password` varchar(256) DEFAULT NULL,
   `gender` varchar(10) DEFAULT NULL,
   `location` varchar(100) DEFAULT NULL,
-  `user_type` varchar(20) DEFAULT NULL,
+  `user_type` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT 'general',
   `image_path` varchar(200) DEFAULT NULL,
   `role_id` varchar(10) DEFAULT NULL,
   `user_role` varchar(100) DEFAULT NULL,
@@ -214,13 +229,14 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`cid`,`pid`,`user_id`,`first_name`,`last_name`,`full_name`,`email`,`mobile`,`password`,`gender`,`location`,`user_type`,`image_path`,`role_id`,`user_role`,`status`,`device_id`,`sync_code`,`app_version`,`otp_token`,`token_expire_time`,`otp_status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
-(1,'TCL','cpanel','1001','super','admin','super admin','admin@gmail.com','+8801710000001','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Male','Gulshan 2','admin',NULL,'1','super_admin',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(2,'PALO','surjo','1002','normal','user','normal user','normal@transcombd.com','12345678','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Male','asdfgn','permanent','default_profile.png','3','normal_user',1,'-',0,NULL,0,NULL,0,'',0,'','2025-07-16 16:24:26',NULL,'2025-07-16 16:44:21',NULL);
+insert  into `users`(`id`,`cid`,`pid`,`user_id`,`first_name`,`last_name`,`full_name`,`email`,`username`,`mobile`,`password`,`gender`,`location`,`user_type`,`image_path`,`role_id`,`user_role`,`status`,`device_id`,`sync_code`,`app_version`,`otp_token`,`token_expire_time`,`otp_status`,`field1`,`field2`,`note`,`created_on`,`created_by`,`updated_on`,`updated_by`) values 
+(1,'TCL','ams','1001','System',' Admin','System  Admin','sysadmin@gmail.com','sysadmin','+8801710000001','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','None','Gulshan 2','sysadmin','default_profile.png','1','system_admin',1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2025-09-11 17:22:54',NULL),
+(7,'TCL','ams','1002','Admin','','Admin ','admin@gmail.com','admin','012589655166','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','None','Gulshan 2','admin','default_profile.png','2','admin',1,'-',0,NULL,0,NULL,0,'',0,'','2025-09-11 17:24:05',NULL,'2025-09-11 17:24:05',NULL),
+(8,'SKF','expense','1003','Expense ','Admin','Expense  Admin','root@gmail.com','stnmy','011111111111','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','None','DHK','sysadmin','default_profile.png','3','sysadmin',1,'-',0,NULL,0,NULL,0,'',0,'','2025-09-11 12:30:19',NULL,'2025-09-11 12:30:19',NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
