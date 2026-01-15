6946b3c79f7daaf25ae7ac4aaa0b367d9d9e45ee
Tamim <tamim.hossen@transcombd.com>
2026-01-15 16:16:00 +0600

    final

--- models/common_cid.py
+++ models/common_cid.py
@@ -3,7 +3,7 @@ import urllib
 db = DAL('mysql://access_management:TJ544#20&^bdsjy@34.143.171.140/access_management', decode_credentials=True)
 date_fixed=datetime.datetime.now()+datetime.timedelta(hours=6)
 
-session.connect(request, response, db, migrate=True)
+session.connect(request, response, db, migrate=False)
 
 # this is for change trucking file without restart the server
 from gluon.custom_import import track_changes
